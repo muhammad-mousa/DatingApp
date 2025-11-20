e6dd91c352d2b0b9ffcdd855644e0e35076d09d0
Mousa Naeem <ranamousa030397@hotmail.com>
2025-11-20 23:00:07 +0500

    End of Section 5 and made the register functional

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,30 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
+import { Register } from '../../features/account/register/register';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
-private http = inject(HttpClient);
-currentUser = signal<User | null>(null);
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
 
-baseUrl = "https://localhost:7292/api/";
+  baseUrl = "https://localhost:7292/api/";
 
-login(creds: any) {
-  return this.http.post<User>(this.baseUrl + "account/login", creds).pipe(
-    tap(user => {
-      if(user){
-        localStorage.setItem('user', JSON.stringify(user));
-        this.currentUser.set(user);
-      }
-    })
-  )
-}
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
 
-logout() {
-  localStorage.removeItem('user');
-  this.currentUser.set(null);
-}
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + "account/login", creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
 }
--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RegisterCreds, User } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +10,22 @@ import { RegisterCreds, User } from '../../../types/user';
   styleUrl: './register.css',
 })
 export class Register {
-membersFromHome = input.required<User[]>();
+  private accountService = inject(AccountService);
 cancelRegister = output<boolean>();
 protected creds = {} as RegisterCreds;
 
-protected register(): void {
-  console.log('Registering user with credentials:', this.creds);
+protected register(){
+  this.accountService.register(this.creds).subscribe({
+    next: user => {
+      console.log('Registration successful', user);
+      if (user) {
+        this.cancel();
+      }
+    },
+    error: err => {
+      console.error('Registration failed', err);
+    }
+  });
 }
 
 cancel() {
