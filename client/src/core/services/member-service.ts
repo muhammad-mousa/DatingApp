5180a24bf5addf8498a91b113216545f1e164d40
Muhammad Mousa <ranamousa030397@gmail.com>
2025-12-21 21:26:20 +0500

    #18 updated the nav bar and add edit button

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member, Photo } from '../../types/member';
 import { AccountService } from './account-service';
@@ -9,8 +9,8 @@ import { AccountService } from './account-service';
 })
 export class MemberService {
   private http = inject(HttpClient);
-  private accountService = inject(AccountService);
   private baseUrl = environment.apiUrl;
+  editMode = signal<boolean>(false);
 
   getMembers() {
     return this.http.get<Member[]>(this.baseUrl + 'members');
