cd34f7c06b3390422f82312248d82d9b7f514ea3
Mousa Naeem <ranamousa030397@hotmail.com>
2025-12-25 17:08:05 +0500

    #18 added the edit profile func to save changes temp and added deactivate guard

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -1,24 +1,38 @@
-import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
+import { Component, HostListener, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
 import { MemberService } from '../../../core/services/member-service';
-import { NgForm } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css',
 })
 export class MemberProfile implements OnInit, OnDestroy {
   @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event: BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  }
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private toast = inject(ToastService);
   protected member = signal<Member | undefined>(undefined);
-  protected editableMember?: EditableMember;
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  }
+
+  constructor() {
+  }
+
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe({
@@ -26,6 +40,7 @@ export class MemberProfile implements OnInit, OnDestroy {
         this.member.set(data['member']);
       }
     });
+
     this.editableMember = {
       displayName: this.member()?.displayName || '',
       description: this.member()?.description || '',
@@ -43,7 +58,7 @@ export class MemberProfile implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    if(this.memberService.editMode()) {
+    if (this.memberService.editMode()) {
       this.memberService.editMode.set(false);
     }
   }
