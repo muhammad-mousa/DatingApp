bfcbeb7424129abae9bc0361ebc5feb86ad61a7f
Mousa Naeem <ranamousa030397@hotmail.com>
2025-12-29 10:49:56 +0500

    #20 update photo and added image upload component

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -10,7 +10,7 @@ import { tap } from 'rxjs';
 export class MemberService {
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
-  editMode = signal<boolean>(false);
+  editMode = signal<boolean>(true);
   member = signal<Member | null>(null);
 
   getMembers() {
