a7485e68458aeb6982e092383e1834f0b370ba12
Mousa Naeem <ranamousa030397@hotmail.com>
2026-01-03 20:32:09 +0500

    #20 added image upload functioanlity

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { MemberService } from '../../../core/services/member-service';
@@ -12,22 +12,33 @@ import { ImageUpload } from "../../../shared/image-upload/image-upload";
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
-export class MemberPhotos {
+export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
-  protected photos$?: Observable<Photo[]>;
+  protected photos = signal<Photo[]>([]);
+  protected loading = signal(false);
 
-  constructor() {
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id');
     if (memberId) {
-      this.photos$ = this.memberService.getMemberPhotos(memberId);
+      this.memberService.getMemberPhotos(memberId).subscribe({
+        next: photos => this.photos.set(photos)
+      });
     }
   }
 
-  get photoMocks() {
-    return Array.from({ length: 20 }, (_, i) => ({
-      url: '/user.png'
-      // url: `https://picsum.photos/200/300?random=${i + 1}`,
-    }));
+  onUploadImage(file: File) {
+    this.loading.set(true);
+    this.memberService.uploadPhoto(file).subscribe({
+      next: photo => {
+        this.memberService.editMode.set(false);
+        this.loading.set(false);
+        this.photos.update(photos => [...photos, photo]);
+      },
+      error: error => {
+        console.error('Photo upload failed', error);
+        this.loading.set(false)
+      }
+    });
   }
 }
